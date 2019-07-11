0ae49f3eae3d8a63c4503628cfaf69395cd7fa21
Nathanael LABREUIL <alabreui@e2r11p16.42.fr>
2019-07-11 16:03:30 +0200

    Add ex02

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 16:00:18 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/11 16:02:24 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	argc--;
+	while (argc > 0)
+	{
+		ft_putstr(argv[argc]);
+		write(1, "\n", 1);
+		argc--;
+	}
+}
+
